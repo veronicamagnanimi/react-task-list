0ce453c69bbeddb7b9c5f1b3d3f260ef09ee7bfc
veronicamagnanimi <veronica.magnanimi@hotmail.it>
2024-12-11 17:10:24 +0100

    feat: base exercise completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,45 @@
 import tasks from "../tasks";
 console.log(tasks);
 
+//array filtrati
 function App() {
-  const inBack = tasks.filter((curItem) => curItem.state === "in_progess" || curItem.state === "backlog");
+  const inBack = tasks.filter((curItem) => curItem.state !== "completed");
   console.log(inBack);
   const completed = tasks.filter((curItem) => curItem.state === "completed");
   console.log(completed);
-  
+
+//per ogni array filtrato creo la lista
+const list = (array) => {
+  return array.map(curItem => {
+    return <li key={curItem.title}>
+      <p>{curItem.priority}</p>
+      <p>{curItem.estimatedTime}</p>
+    </li>
+  })
+}
+
+
 
   return (
     <>
-      <h1>Task List</h1>
+      <header>
+      <h1>Task Manager</h1>
+      </header>
+
+      <main>
+      <h2>Current task(4)</h2>
+      <ul>
+       {list(inBack)}
+      </ul>
+
+      <hr></hr>
+
+      <h2>Completed Task(6)</h2>
+      <ul>
+        {list(completed)}
+      </ul>
+      </main>
+    
     </>
   )
 }
