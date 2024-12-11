1bdc46a27aee65ea6bacbf1583404aa5c1c97338
veronicamagnanimi <veronica.magnanimi@hotmail.it>
2024-12-11 16:43:59 +0100

    feat: filtered array

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import tasks from "../tasks";
+console.log(tasks);
 
 function App() {
-  const [count, setCount] = useState(0)
+  const inBack = tasks.filter((curItem) => curItem.state === "in_progess" || curItem.state === "backlog");
+  console.log(inBack);
+  const completed = tasks.filter((curItem) => curItem.state === "completed");
+  console.log(completed);
+  
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Task List</h1>
     </>
   )
 }
